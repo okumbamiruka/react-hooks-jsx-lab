9167c6b5ad052356d23dd634d2099d2534343cb8
Chrispine Oburu <chrizoburu65@gmail.com>
2024-04-23 11:49:11 +0300

    test

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { image } from "../data/data";
+import {name, city, image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I am {name} and I am from {city}.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  )
 }
 
 export default About;
